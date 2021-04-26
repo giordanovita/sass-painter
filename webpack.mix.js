ba7fc9276d6bd03bef9d78a24a87e78923c3709a
giordano <giordanovitag@libero.it>
2021-04-26 17:39:38 +0200

    step 2

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,6 +2,10 @@ let mix = require('laravel-mix');
 mix
     .js('src/app.js', 'dist')
     .sass('src/style.scss', '')
+    .options( {
+       
+        processCssUrls: false,
+    })
     .setPublicPath('dist')
     .copy('src/index.html','dist')
     ;
